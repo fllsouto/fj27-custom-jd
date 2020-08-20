b468eafafcfb33cfc644d63bb4b608ca7f9855bc
Fellipe Souto <fllsouto@gmail.com>
2020-08-20 16:42:32 -0300

    Setup inicial projeto

---
+++ src/main/java/br/com/caelum/casadocodigoapi/CasadocodigoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.caelum.casadocodigoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasadocodigoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasadocodigoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/caelum/casadocodigoapi/CasadocodigoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caelum.casadocodigoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasadocodigoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
